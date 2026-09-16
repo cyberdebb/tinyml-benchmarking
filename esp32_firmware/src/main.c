f598d2af3b7fa3e3794024986b39acccd7731ae1
cyberdebb <deboramoreiracastro@gmail.com>
2026-09-16 18:14:41 -0300

    testing new configs

--- esp32_firmware/src/main.c
+++ esp32_firmware/src/main.c
@@ -1,6 +1,25 @@
-extern void tinyml_app_main(void);
+// extern void tinyml_app_main(void);
 
-void app_main(void)
-{
-    tinyml_app_main();
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_heap_caps.h"
+
+void app_main(void) {
+    // tinyml_app_main();
+    printf("Inicializando ESP32-S3...\n");
+
+    // Verifica a quantidade de memória PSRAM disponível
+    size_t psram_free = heap_caps_get_free_size(MALLOC_CAP_SPIRAM);
+    printf("Memoria PSRAM livre: %d bytes\n", psram_free);
+
+    if (psram_free == 0) {
+        printf("ALERTA: PSRAM nao foi alocada!\n");
+    }
+
+    // Loop infinito do FreeRTOS
+    while(1) {
+        printf("Sistema rodando...\n");
+        vTaskDelay(2000 / portTICK_PERIOD_MS); // Pausa de 2 segundos
+    }
 }
