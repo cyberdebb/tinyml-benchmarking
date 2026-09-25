f908473e2d74d13d2e4801b7f4e1917234526400
cyberdebb <deboramoreiracastro@gmail.com>
2026-09-25 20:32:55 -0300

    fixing multiple calls error

--- stm32_firmware/src/tinyml_app.cc
+++ stm32_firmware/src/tinyml_app.cc
@@ -2,21 +2,11 @@
 #include <cstdint>
 #include <cstdio>
 
-#include "stm32f7xx_hal.h"
-
+#include "board.h"
 #include "tinyml_common.h"
 #include "tinyml_model.h"
 
 #define DWT_LAR_UNLOCK_KEY 0xC5ACCE55
-
-extern UART_HandleTypeDef huart3;
-
-// printf goes to the UART (newlib calls _write).
-extern "C" int _write(int file, char *ptr, int len)
-{
-    HAL_UART_Transmit(&huart3, (uint8_t *)ptr, len, HAL_MAX_DELAY);
-    return len;
-}
 
 namespace {
 
